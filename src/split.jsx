b00c6f295339dbd5c651365cfd1c253d86d16285
James Hrisho <james.hrisho@gmail.com>
2017-05-22 17:42:05 -0400

    Allow for split and in the future other components

---
+++ src/split.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SplitComponent extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default SplitComponent;
