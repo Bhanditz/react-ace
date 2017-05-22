d79a3232f839acab41b22b591ef59d5e1d803dd4
James Hrisho <james.hrisho@gmail.com>
2017-05-22 17:38:31 -0400

    Example should also have dist available

--- webpack.config.example.js
+++ webpack.config.example.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   devServer: {
     hot: true,
-    contentBase: path.join(__dirname, 'example'),
+    contentBase:  [path.join(__dirname, 'example'), path.join(__dirname, 'dist')],
     compress: true,
     port: 9000,
   },
